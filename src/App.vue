eaaa96e364af0110c461c68964a69f1321cb5ec0
userName <userEmail>
2025-06-15 17:44:03 +0800

    修改App页面内容

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
+    <h1>你好啊</h1>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
